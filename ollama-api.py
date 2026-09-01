72fd421986ec6533dd37ce512f902ea86c44b193
kishan-proiq <kishan.m@toadsters.com>
2026-09-01 15:48:53 +0530

    ollama updated V3

--- ollama-api.py
+++ ollama-api.py
@@ -22,7 +22,7 @@ async def infer(request: Request):
                             if line:
                                 await queue.put(line)
             except Exception as e:
-                await queue.put(json.dumps({"error caught ": str(e)}))
+                await queue.put(json.dumps({"error caught while executing the task ": str(e)}))
             finally:
                 await queue.put(None)  
 
